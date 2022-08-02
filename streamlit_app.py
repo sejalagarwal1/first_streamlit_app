fbd7e474e2180352e3f8d432dff42d5fa050b342
sejalagarwal1 <107094001+sejalagarwal1@users.noreply.github.com>
2022-08-02 15:48:10 +0530

    Update streamlit_app.py
    importing pandas 
    reading file
    making data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+#importing pandas lib to make table
 streamlit.title('my parents healthy new dinner')
 streamlit.header('Breakfast favorties')
 streamlit.text('🥣Omega3 and buleberry Oat meal')
@@ -7,3 +9,6 @@ streamlit.text('🐔Hard-boiled free-range egg')
 streamlit.text('🥑🍞Avacodo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#reading the file from s3 bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
