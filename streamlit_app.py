99b9473ed188b5c62a3dcf5eee84973ffd79987b
sejalagarwal1 <107094001+sejalagarwal1@users.noreply.github.com>
2022-08-07 21:51:00 +0530

    Update streamlit_app.py
    correcting changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+
 #importing pandas lib to make table
 streamlit.title('my parents healthy new dinner')
 streamlit.header('Breakfast favorties')
@@ -33,3 +33,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it in the screen as table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
