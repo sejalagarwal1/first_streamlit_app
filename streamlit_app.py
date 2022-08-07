b6710b7fd1468eff1506aeef094a5861a3927c54
sejalagarwal1 <107094001+sejalagarwal1@users.noreply.github.com>
2022-08-07 22:20:57 +0530

    Update streamlit_app.py
    removing everything nd running

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,3 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it in the screen as table
 streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
-
